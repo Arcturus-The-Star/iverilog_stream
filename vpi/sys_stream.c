23e183b561ab9db749ee65fd96a0831c99bc7d76
Arcturus-The-Star <sjavery@willamette.edu>
2026-05-27 13:22:22 -0400

    Changed streaming to use same system call framework as vcd and other such calls

--- vpi/sys_stream.c
+++ vpi/sys_stream.c
@@ -22,12 +22,14 @@ static struct stream_info *stream_dmp_list = NULL;
 static char* stream_server = NULL;
 static rd_kafka_t* producer;
 static char* topic = "iv_data_stream";
+static char* stream_key = NULL;
 static int finish_status = 0;
 static int stream_status = 0;
-static PLI_UINT64 startstream_time;
+static PLI_UINT64 dumpvars_time;
 static struct t_vpi_time zero_delay = { vpiSimTime, 0, 0, 0.0 };
 static PLI_UINT64 stream_cur_time;
 static int dump_no_date = 0;
+static int dump_is_off = 0;
 
 static const char*units_names[] = {
       "s",
@@ -65,14 +67,13 @@ static void gen_new_strid(void)
       assert(0);
 }
 static void send_message(char* msg) {
-	char* key = "IV_STREAM_TEST";
-	int key_len = strlen(key);
+	int key_len = strlen(stream_key);
 	int msg_len = strlen(msg);
 	rd_kafka_resp_err_t err;
 	err = rd_kafka_producev(producer,
 		RD_KAFKA_V_TOPIC(topic),
 		RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
-		RD_KAFKA_V_KEY((void*)key, key_len),
+		RD_KAFKA_V_KEY((void*)stream_key, key_len),
 		RD_KAFKA_V_VALUE((void*)msg, msg_len),
 		RD_KAFKA_V_OPAQUE(NULL),
 		RD_KAFKA_V_END
@@ -82,6 +83,10 @@ static void send_message(char* msg) {
 	}
 
 	rd_kafka_poll(producer, 0);
+}
+
+static inline int dump_header_pending(void) {
+	return stream_status != 2;
 }
 
 static char* truncate_bitvec(char *s) {
@@ -180,10 +185,10 @@ static PLI_INT32 stream_finish_cb(p_cb_data cause) {
 	
 	finish_status = 1;
 
-	startstream_time = timerec_to_time64(cause->time);
+	dumpvars_time = timerec_to_time64(cause->time);
 
-	if (startstream_time != stream_cur_time) {
-		strbuf_append_linef(&message, "#%" PLI_UINT64_FMT "\n", startstream_time);
+	if (dumpvars_time != stream_cur_time && !dump_is_off) {
+		strbuf_append_linef(&message, "#%" PLI_UINT64_FMT "\n", dumpvars_time);
 		send_message(message.data);
 	}
 
@@ -240,42 +245,57 @@ static void show_this_item(struct stream_info *info) {
 
 }
 
-static PLI_INT32 startstream_cb(p_cb_data cause) {
+static void show_this_item_x(struct stream_info *info) {
+	PLI_INT32 type = vpi_get(vpiType, info->item);
+
+	if (type == vpiRealVar) {
+		strbuf_append_linef(&message, "rNan %s\n", info->ident);
+	} else if (type == vpiNamedEvent) {
+		
+	} else if (vpi_get(vpiSize, info->item) == 1) {
+		strbuf_append_linef(&message, "x %s\n", info->ident);
+	} else {
+		strbuf_append_linef(&message, "bx %s\n", info->ident);
+	}
+}
+
+static PLI_INT32 dumpvars_cb(p_cb_data cause) {
 	if (stream_status != 1) return 0;
 
 	stream_status = 2;
 
-	startstream_time = timerec_to_time64(cause->time);
-	stream_cur_time = startstream_time;
+	dumpvars_time = timerec_to_time64(cause->time);
+	stream_cur_time = dumpvars_time;
 	
 	strbuf_append_linef(&message, "$enddefinitions $end\n");
-
-	strbuf_append_linef(&message, "$comment Show the parameter values. $end\n");
-	strbuf_append_linef(&message, "$dumpall\n");
-	ITERATE_VCD_INFO(stream_const_list, stream_info, next, show_this_item);
-	strbuf_append_linef(&message, "$end\n");
-	strbuf_append_linef(&message, "#%" PLI_UINT64_FMT "\n", startstream_time);
-	strbuf_append_linef(&message, "$dumpvars\n");
-	ITERATE_VCD_INFO(stream_list, stream_info, next, show_this_item);
-	strbuf_append_linef(&message, "$end\n");
-	send_message(message.data);
-	strbuf_free(&message);
+	if (!dump_is_off) {
+		strbuf_append_linef(&message, "$comment Show the parameter values. $end\n");
+		strbuf_append_linef(&message, "$dumpall\n");
+		ITERATE_VCD_INFO(stream_const_list, stream_info, next, show_this_item);
+		strbuf_append_linef(&message, "$end\n");
+		strbuf_append_linef(&message, "#%" PLI_UINT64_FMT "\n", dumpvars_time);
+		strbuf_append_linef(&message, "$dumpvars\n");
+		ITERATE_VCD_INFO(stream_list, stream_info, next, show_this_item);
+		strbuf_append_linef(&message, "$end\n");
+		send_message(message.data);
+		strbuf_free(&message);
+	}
 
 	return 0;
 }
 
-__inline__ static int install_startstream_cb(void) {
+__inline__ static int install_dumpvars_cb(void) {
 	struct t_cb_data cb;
 	
 	if (stream_status == 1) return 0;
 	if (stream_status == 2) {
-		vpi_printf("Stream warning: $startstream ignored, previously called at simtime %" PLI_UINT64_FMT "\n", startstream_time);
+		vpi_printf("Stream warning: $dumpvars ignored, previously called at simtime %" PLI_UINT64_FMT "\n", dumpvars_time);
 		return 1;
 	}
 	
 	cb.time = &zero_delay;
 	cb.reason = cbReadOnlySynch;
-	cb.cb_rtn = startstream_cb;
+	cb.cb_rtn = dumpvars_cb;
 	cb.user_data = 0x0;
 	cb.obj = 0x0;
 	vpi_register_cb(&cb);
@@ -308,8 +328,6 @@ static void set_config(vpiHandle callh, rd_kafka_conf_t *conf, char *key, char *
 		return;
 	}
 }
-
-
 
 static void start_producer(vpiHandle callh) {
 	char errstr[512];
@@ -345,6 +363,7 @@ static void start_producer(vpiHandle callh) {
 			prec -= 1;
 		}
 		strbuf_init(&message, 50);
+		stream_key = asctime(localtime(&walltime));
 		if (!dump_no_date) strbuf_append_linef(&message, "$date\n\t%s$end\n", asctime(localtime(&walltime)));
 		strbuf_append_linef(&message, "$version\n\tIcarus Verilog\n$end\n$timescale\n\t%u%s\n$end\n", scale, units_names[udx]);
 		send_message(message.data);
@@ -382,6 +401,7 @@ static PLI_INT32 variable_cb_1(p_cb_data cause) {
 	
 	if (info->scheduled) return 0;
 	if (stream_status != 2) return 0;
+	if (dump_is_off) return 0;
 	
 	if (!stream_dmp_list) {
 		cb = *cause;
@@ -474,7 +494,7 @@ static void scan_item(unsigned depth, vpiHandle item, int skip) {
 	  case vpiTask:       type = "task"; break;
 
 	  default:
-	    vpi_printf("Streaming warning: $startstream: Unsupported argument "
+	    vpi_printf("Streaming warning: $dumpvars: Unsupported argument "
 	               "type (%s).\n", vpi_get_str(vpiType, item));
 	    return;
       }
@@ -605,7 +625,7 @@ static void scan_item(unsigned depth, vpiHandle item, int skip) {
 		break;
 	case vpiPackage:
 		if (vcd_instance_contains_dumpable_items(dumpable_types, item)) {
-			vpi_printf("Stream warning: $startstream: Package (%s) is not dumpable with stream.\n", vpi_get_str(vpiFullName, item));
+			vpi_printf("Stream warning: $dumpvars: Package (%s) is not dumpable with stream.\n", vpi_get_str(vpiFullName, item));
 		}
 		break;
 
@@ -634,7 +654,7 @@ static int draw_scope(vpiHandle item, vpiHandle callh) {
 		case vpiModule:      type = "module";     break;
 		default:
 			type = "invalid";
-			vpi_printf("Stream error: %s:%d: $enablestream: Unsupported scope type (%d)\n",
+			vpi_printf("Stream error: %s:%d: $dumpfile: Unsupported scope type (%d)\n",
 					vpi_get_str(vpiFile, callh),
 					(int)vpi_get(vpiLineNo, callh),
 					(int)vpi_get(vpiType, item));
@@ -648,7 +668,7 @@ static int draw_scope(vpiHandle item, vpiHandle callh) {
 
 
 
-static PLI_INT32 sys_enablestream_calltf(ICARUS_VPI_CONST PLI_BYTE8*name) {
+static PLI_INT32 sys_dumpfile_calltf(ICARUS_VPI_CONST PLI_BYTE8*name) {
 	(void) name;
 	s_vpi_value val;
 	vpiHandle callh = vpi_handle(vpiSysTfCall, 0);
@@ -660,7 +680,7 @@ static PLI_INT32 sys_enablestream_calltf(ICARUS_VPI_CONST PLI_BYTE8*name) {
 	return 0;
 }
 
-static PLI_INT32 sys_startstream_calltf(ICARUS_VPI_CONST PLI_BYTE8*name) {
+static PLI_INT32 sys_dumpvars_calltf(ICARUS_VPI_CONST PLI_BYTE8*name) {
 	(void) name;
 	vpiHandle callh = vpi_handle(vpiSysTfCall, 0);
     vpiHandle argv = vpi_iterate(vpiArgument, callh);
@@ -676,7 +696,7 @@ static PLI_INT32 sys_startstream_calltf(ICARUS_VPI_CONST PLI_BYTE8*name) {
 		}
 	}
 	
-	if (install_startstream_cb()) {
+	if (install_dumpvars_cb()) {
 		if (argv) vpi_free_object(argv);
 		return 0;
 	}
@@ -753,7 +773,7 @@ static PLI_INT32 sys_startstream_calltf(ICARUS_VPI_CONST PLI_BYTE8*name) {
 	return 0;
 }
 
-static PLI_INT32 sys_streamflush_calltf(ICARUS_VPI_CONST PLI_BYTE8 *name) {
+static PLI_INT32 sys_dumpflush_calltf(ICARUS_VPI_CONST PLI_BYTE8 *name) {
 	(void)name;
 	if (producer) {
 		rd_kafka_flush(producer, 1000 * 100);
@@ -761,6 +781,95 @@ static PLI_INT32 sys_streamflush_calltf(ICARUS_VPI_CONST PLI_BYTE8 *name) {
 	return 0;
 }
 
+static PLI_INT32 sys_dumpall_calltf(ICARUS_VPI_CONST PLI_BYTE8 *name) {
+	s_vpi_time now;
+	PLI_UINT64 now64;
+
+	(void)name;
+	
+	if (!dump_is_off) return 0;
+	if (!producer) return 0;
+	if (dump_header_pending()) return 0;
+
+	now.type = vpiSimTime;
+	vpi_get_time(0, &now);
+	now64 = timerec_to_time64(&now);
+
+	strbuf_init(&message, 50);
+
+	if (now64 > stream_cur_time) {
+		strbuf_append_linef(&message, "#%" PLI_UINT64_FMT "\n", now64);
+		stream_cur_time = now64;
+	}
+
+	strbuf_append_linef(&message, "$dumpall\n");
+	ITERATE_VCD_INFO(stream_list, stream_info, next, show_this_item);
+	strbuf_append_linef(&message, "$end\n");
+	send_message(message.data);
+	strbuf_free(&message);
+	return 0;
+}
+
+static PLI_INT32 sys_dumplimit_calltf(ICARUS_VPI_CONST PLI_BYTE8 *name) {
+	(void)name;
+	printf("WARNING: dumplimit has no effect on streaming");
+	return 0;
+}
+
+static PLI_INT32 sys_dumpon_calltf(ICARUS_VPI_CONST PLI_BYTE8 *name) {
+	s_vpi_time now;
+	PLI_UINT64 now64;
+
+	(void)name;
+
+	if (!dump_is_off) return 0;
+
+	dump_is_off = 0;
+	if (!producer) return 0;
+	if (dump_header_pending()) return 0;
+	now.type = vpiSimTime;
+    vpi_get_time(0, &now);
+    now64 = timerec_to_time64(&now);
+
+    if (now64 > stream_cur_time) {
+		strbuf_append_linef(&message, "#%" PLI_UINT64_FMT "\n", now64);
+		stream_cur_time = now64;
+    }
+
+    strbuf_append_linef(&message, "$dumpon\n");
+    ITERATE_VCD_INFO(stream_list, stream_info, next, show_this_item);
+    strbuf_append_linef(&message, "$end\n");
+	return 0;
+}
+
+static PLI_INT32 sys_dumpoff_calltf(ICARUS_VPI_CONST PLI_BYTE8 *name) {
+	s_vpi_time now;
+	PLI_UINT64 now64;
+
+	(void)name;
+	if (dump_is_off) return 0;
+
+	dump_is_off = 1;
+
+	if (!producer) return 0;
+	if (dump_header_pending()) return 0;
+	strbuf_init(&message, 50);
+
+	now.type = vpiSimTime;
+	vpi_get_time(0, &now);
+	now64 = timerec_to_time64(&now);
+	
+	if (now64 > stream_cur_time) {
+		strbuf_append_linef(&message, "#%" PLI_UINT64_FMT "\n", now64);
+		stream_cur_time = now64;
+	}
+	strbuf_append_linef(&message, "$dumpoff\n");
+	ITERATE_VCD_INFO(stream_list, stream_info, next, show_this_item_x);
+	strbuf_append_linef(&message, "$end\n");
+	send_message(message.data);
+	strbuf_free(&message);
+	return 0;
+}
 
 void sys_stream_register(void) {
 	s_vpi_systf_data tf_data;
@@ -776,29 +885,65 @@ void sys_stream_register(void) {
 	}
 
 	tf_data.type = vpiSysTask;
-	tf_data.tfname = "$enablestream";
-	tf_data.calltf = sys_enablestream_calltf;
+	tf_data.tfname = "$dumpfile";
+	tf_data.calltf = sys_dumpfile_calltf;
 	tf_data.compiletf = sys_one_string_arg_compiletf;
 	tf_data.sizetf = 0;
-	tf_data.user_data = "$enablestream";
+	tf_data.user_data = "$dumpfile";
 	res = vpi_register_systf(&tf_data);
 	vpip_make_systf_system_defined(res);
 
 	tf_data.type = vpiSysTask;
-	tf_data.tfname = "$startstream";
-	tf_data.calltf = sys_startstream_calltf;
+	tf_data.tfname = "$dumpvars";
+	tf_data.calltf = sys_dumpvars_calltf;
 	tf_data.compiletf = sys_dumpvars_compiletf;
 	tf_data.sizetf = 0;
-	tf_data.user_data = "$startstream";
+	tf_data.user_data = "$dumpvars";
 	res = vpi_register_systf(&tf_data);
 	vpip_make_systf_system_defined(res);
 
 	tf_data.type = vpiSysTask;
-	tf_data.tfname = "$streamflush";
-	tf_data.calltf = sys_streamflush_calltf;
+	tf_data.tfname = "$dumpflush";
+	tf_data.calltf = sys_dumpflush_calltf;
 	tf_data.compiletf = sys_no_arg_compiletf;
 	tf_data.sizetf = 0;
-	tf_data.user_data = "$streamflush";
+	tf_data.user_data = "$dumpflush";
+	res = vpi_register_systf(&tf_data);
+	vpip_make_systf_system_defined(res);
+
+	tf_data.type      = vpiSysTask;
+	tf_data.tfname    = "$dumpall";
+	tf_data.calltf    = sys_dumpall_calltf;
+	tf_data.compiletf = sys_no_arg_compiletf;
+	tf_data.sizetf    = 0;
+	tf_data.user_data = "$dumpall";
+	res = vpi_register_systf(&tf_data);
+	vpip_make_systf_system_defined(res);
+
+	tf_data.type      = vpiSysTask;
+	tf_data.tfname    = "$dumplimit";
+	tf_data.calltf    = sys_dumplimit_calltf;
+	tf_data.compiletf = sys_one_numeric_arg_compiletf;
+	tf_data.sizetf    = 0;
+	tf_data.user_data = "$dumplimit";
+	res = vpi_register_systf(&tf_data);
+	vpip_make_systf_system_defined(res);
+
+	tf_data.type      = vpiSysTask;
+	tf_data.tfname    = "$dumpoff";
+	tf_data.calltf    = sys_dumpoff_calltf;
+	tf_data.compiletf = sys_no_arg_compiletf;
+	tf_data.sizetf    = 0;
+	tf_data.user_data = "$dumpoff";
+	res = vpi_register_systf(&tf_data);
+	vpip_make_systf_system_defined(res);
+
+	tf_data.type      = vpiSysTask;
+	tf_data.tfname    = "$dumpon";
+	tf_data.calltf    = sys_dumpon_calltf;
+	tf_data.compiletf = sys_no_arg_compiletf;
+	tf_data.sizetf    = 0;
+	tf_data.user_data = "$dumpon";
 	res = vpi_register_systf(&tf_data);
 	vpip_make_systf_system_defined(res);
 
